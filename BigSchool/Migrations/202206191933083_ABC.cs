48993f744896c4624f34e3fd86dc90140ab5229f
MacBook Pro <MacBook Pro@DESKTOP-K7J5H0E>
2022-06-20 07:45:13 +0700

    Add project files.

---
+++ BigSchool/Migrations/202206191933083_ABC.cs
@@ -0,0 +1,20 @@
+﻿namespace BigSchool.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ABC : DbMigration
+    {
+        public override void Up()
+        {
+            RenameColumn(table: "dbo.Courses", name: "Lecturer_Id", newName: "LecturerId");
+            RenameIndex(table: "dbo.Courses", name: "IX_Lecturer_Id", newName: "IX_LecturerId");
+        }
+        
+        public override void Down()
+        {
+            RenameIndex(table: "dbo.Courses", name: "IX_LecturerId", newName: "IX_Lecturer_Id");
+            RenameColumn(table: "dbo.Courses", name: "LecturerId", newName: "Lecturer_Id");
+        }
+    }
+}
